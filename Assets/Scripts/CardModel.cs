d483f08325d08b44b4989f6739777bd2104b46d1
Hernando NJ <40243888+HernandoNJ@users.noreply.github.com>
2021-09-26 23:00:22 -0500

    Cards ok

--- Assets/Scripts/CardModel.cs
+++ Assets/Scripts/CardModel.cs
@@ -6,7 +6,7 @@ public class CardModel : ScriptableObject
     public string title;
     public string description;
     public string exhibit;
-    public Sprite animalImage;
+    public Sprite animalSprite;
 }
 
 
--- Assets/Scripts/CardView.cs
+++ Assets/Scripts/CardView.cs
@@ -7,6 +7,16 @@ public class CardView : MonoBehaviour
     public Text description;
     public Text exhibit;
     public Image animalImage;
+
+    public CardModel[] cards;
+
+    public void DisplayCard(int cardIndex)
+    {
+        title.text = cards[cardIndex].title;
+        description.text = cards[cardIndex].description;
+        exhibit.text = cards[cardIndex].exhibit;
+        animalImage.sprite = cards[cardIndex].animalSprite;
+    }
 }
 
 
