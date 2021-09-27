26023541c3a47810af7087d3732a650e0ecf1989
Hernando NJ <40243888+HernandoNJ@users.noreply.github.com>
2021-09-26 22:08:28 -0500

    Progress in Zoo cards

---
+++ Assets/Scripts/CardModel.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card/ZooCard")]
+public class CardModel : ScriptableObject
+{
+    public string title;
+    public string description;
+    public string exhibit;
+    public Sprite animalImage;
+}
+
+
---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour
+{
+    public Text title;
+    public Text description;
+    public Text exhibit;
+    public Image animalImage;
+}
+
+
+
+
